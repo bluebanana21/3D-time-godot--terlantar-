3e064d0533d4e8ecbe4ba8406c58dd664c63b482
bluebanana21 <m.affanfahrozi@gmail.com>
2024-03-08 19:29:34 +0700

    added player & camera movement

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -3,10 +3,30 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+@export var SENSITIVITY = 0.03
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+#camera variables
+@onready var head := $Head
+@onready var camera := $Head/Camera3D
+
+
+#removes mouse cursor
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+#Camera controls
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		
+		#Limits camera movement vertically
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(0), deg_to_rad(0))
+	
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,13 +39,13 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("Left", "Right", "Forward", "Down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = 0.0
+		velocity.z = 0.0
 
 	move_and_slide()
