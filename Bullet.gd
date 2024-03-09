7aa1a6717f4a826c04a6047aed60d351e5f0f89c
bluebanana21 <m.affanfahrozi@gmail.com>
2024-03-09 20:29:26 +0700

    added shooting

---
+++ Bullet.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+
+const SPEED = 40
+
+@onready var mesh = $MeshInstance3D
+@onready var ray = $RayCast3D
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	position += transform.basis * Vector3(0, 0, -SPEED) * delta
--- Scripts/CharacterBody3D.gd
+++ Scripts/CharacterBody3D.gd
@@ -8,21 +8,26 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+#Bullets
+var bullet = load("res://bullet.tscn")
+var instance
+
 #camera variables
 @onready var head := $Head
 @onready var eyes := $Head/Eyes
 @onready var camera := $Head/Eyes/Camera3D
+@onready var gun_barrel = $Head/Eyes/Camera3D/Rifle
 
 #Head bobbing vars
-const Head_bobbing_sprinting_speed = 22.0
-const Head_bobbing_walking_speed = 14.0
-
-const head_bobing_sprinting_intensity = 0.2
-const head_bobing_walking_intensity = 0.1
-
-var head_bobbing_vector = Vector2.ZERO
-var head_bobbing_index = 0.0
-var head_bobbing_current_intensity = 0.0
+#const Head_bobbing_sprinting_speed = 22.0
+#const Head_bobbing_walking_speed = 14.0
+#
+#const head_bobing_sprinting_intensity = 0.2
+#const head_bobing_walking_intensity = 0.1
+#
+#var head_bobbing_vector = Vector2.ZERO
+#var head_bobbing_index = 0.0
+#var head_bobbing_current_intensity = 0.0
 
 #removes mouse cursor
 func _ready():
@@ -39,10 +44,6 @@ func _unhandled_input(event):
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(0), deg_to_rad(0))
 	
 	#Checking shooting input responsiveness
-	if Input.is_action_just_pressed("Shoot"):
-		print("You shot someone")
-	if Input.is_action_just_pressed("Reload"):
-		print("you realoaded")
 
 
 func _physics_process(delta):
@@ -66,14 +67,20 @@ func _physics_process(delta):
 		velocity.z = 0.0
 	
 	#Head bobbing logic
-	if input_dir != Vector2.ZERO:
-		head_bobbing_current_intensity = head_bobing_walking_intensity
-		head_bobbing_index += Head_bobbing_walking_speed * delta
-	
-	if is_on_floor() && input_dir != Vector2.ZERO:
-		head_bobbing_vector.y = sin(head_bobbing_index)
-		head_bobbing_vector.x = sin(head_bobbing_index / 2) + 0.5
+	#if input_dir != Vector2.ZERO:
+		#head_bobbing_current_intensity = head_bobing_walking_intensity
+		#head_bobbing_index += Head_bobbing_walking_speed * delta
+	#
+	#if is_on_floor() && input_dir != Vector2.ZERO:
+		#head_bobbing_vector.y = sin(head_bobbing_index)
+		#head_bobbing_vector.x = sin(head_bobbing_index / 2) + 0.5
 		
 		#eyes.position.y = lerp(eyes.position.y, head_bobbing_vector.y * (head_bobbing_current_intensity / 2.0), delta*lerp)
+	
+	if Input.is_action_pressed("Shoot"):
+		instance = bullet.instantiate()
+		instance.position = gun_barrel.global_position
+		instance.transform.basis = gun_barrel.global_transform.basis
+		get_parent().add_child(instance)
 	
 	move_and_slide()
