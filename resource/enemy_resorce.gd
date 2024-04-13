d08109092bbe205592201d371ec2050003f610ea
bluebanana21 <m.affanfahrozi@gmail.com>
2024-04-13 21:06:48 +0700

    added resource

---
+++ resource/enemy_resorce.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name Enemy
+
+@export var name : String
+@export var move_speed : float
+@export var attack_range : float
+@export var projectile_damage : int
+@export var damage_power : int
+@export var enemy_health : int
+@export var can_shoot : bool
+@export var can_attack : bool
